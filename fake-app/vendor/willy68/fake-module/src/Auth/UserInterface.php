f24e1659f81b4de3f715c8fba636fa61231f2b11
William Lety <william.lety@gmail.com>
2023-03-26 14:43:45 +0200

    Initial commit

---
+++ fake-app/vendor/willy68/fake-module/src/Auth/UserInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgFramework\FakeModule\Auth;
+
+interface UserInterface
+{
+
+}
---
+++ fake-app/vendor/willy68/fake-module/src/RouterModule.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgFramework\FakeModule;
+
+use PgFramework\Module;
+
+class RouterModule extends Module
+{
+
+}
