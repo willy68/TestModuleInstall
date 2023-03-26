6d7cb609409b24095292ab58572fa3bca4ff0a38
William Lety <william.lety@gmail.com>
2023-03-26 15:02:25 +0200

    Initial commit

---
+++ fake-module/vendor/willy68/pgmodule/src/Module.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgFramework;
+
+class Module
+{
+    public const DEFINITIONS = null;
+
+    public const MIGRATIONS = null;
+
+    public const SEEDS = null;
+
+    public const ANNOTATIONS = [];
+}
