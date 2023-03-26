7793f792c883223188ffa7f40d87238f03caccec
William Lety <william.lety@gmail.com>
2023-03-26 15:16:11 +0200

    Initial commit

---
+++ fake-app/vendor/willy68/fake-module/src/Auth/AuthModule.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgFramework\FakeModule\Auth;
+
+use PgFramework\Module;
+
+class AuthModule extends  Module
+{
+
+}
---
+++ fake-app/vendor/willy68/fake-module/src/FakeModule.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgFramework\FakeModule;
+
+use PgFramework\Module;
+
+class FakeModule extends Module
+{
+
+}
